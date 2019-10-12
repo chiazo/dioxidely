4f3f494834f22e4eb01e9cdb70105b6ba1f3a378
chiazo <chiazo@live.unc.edu>
2019-10-12 14:53:11 -0400

    added dependencies for real time build

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,14 @@
-import express from "express";
+import express, { response } from "express";
 import request from "request";
 // import { sequelize } from "../db/sequelize";
 
 const router = express.Router();
+
+// req, res
+router.get("/helloworld", (req, res) => {
+    const name = "hello world";
+    // send a json obj
+    res.send(name);
+});
 
 export default router;
