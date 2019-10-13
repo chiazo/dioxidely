3bf825ee9d3f402010be3cf9c65ecfd02c0cc6ea
Visrut Sudhakar <visrut@gmail.com>
2019-10-13 00:59:39 -0400

    Using env vars for port number

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { app } from "./app";
 
-app.listen(8000, () => {
-    console.log(`Server started on port ${8000}`);
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
